c1ecf9f212398cb57d5a5ff3c73151df1957294a
jenises5 <mariquitjenises05@gmail.com>
2025-12-09 20:32:22 +0800

    secondcommit

---
+++ database/migrations/2025_12_09_115627_create_courses_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\ValidationData;
+
+return new class extends Migration
+{
+  public function up(): void 
+  {
+    Schema::create ('courses', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('instructor_id')->constrained('users')->oneDelete('cascade');
+        $table->string('title');
+        $table->text('description');
+        $table->longtext('content');
+        $table->timestamps();
+    });
+  }
+  
+};
