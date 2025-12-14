3457c71286939037c797190e0bc4ebded99aaf04
jenises5 <mariquitjenises05@gmail.com>
2025-12-14 15:03:45 +0800

    todeploy

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,21 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/',
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    build: {
+        manifest: 'manifest.json',
+        outDir: 'public/build',
+        rollupOptions: {
+            input: {
+                app: 'resources/js/app.js',
+                css: 'resources/css/app.css',
+            }
+        }
+    }
 });
